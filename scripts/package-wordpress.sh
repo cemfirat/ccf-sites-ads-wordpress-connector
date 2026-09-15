3035ee0257038b66b2612c42faa3da26ab617734
Cem Firat <5923877+cemfirat@users.noreply.github.com>
2026-09-15 11:05:55 +0200

    Package content administration module

--- scripts/package-wordpress.sh
+++ scripts/package-wordpress.sh
@@ -14,6 +14,7 @@ cp "$ROOT_DIR/wordpress/readme.txt" "$PLUGIN_DIR/"
 cp "$ROOT_DIR/wordpress/README.md" "$PLUGIN_DIR/README.md"
 cp "$ROOT_DIR/wordpress/assets/ccf-tracking.js" "$PLUGIN_DIR/assets/"
 cp "$ROOT_DIR/wordpress/includes/class-ccf-sites-control.php" "$PLUGIN_DIR/includes/"
+cp "$ROOT_DIR/wordpress/includes/class-ccf-sites-content-admin.php" "$PLUGIN_DIR/includes/"
 
 cd "$BUILD_DIR"
 zip -qr "$ZIP_PATH" ccf-google-ads-site-connector
