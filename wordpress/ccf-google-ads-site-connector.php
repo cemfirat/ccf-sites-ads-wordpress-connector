534c98a63c13dab71c9e192decf0cb44e9cb886e
Cem Firat <5923877+cemfirat@users.noreply.github.com>
2026-09-17 14:09:03 +0200

    Initialize author discovery and bump to 1.3.1

--- wordpress/ccf-google-ads-site-connector.php
+++ wordpress/ccf-google-ads-site-connector.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: CCF Sites & Ads Connector
  * Description: Secure WordPress control, content administration and conversion connector for CCF Sites & Ads.
- * Version: 1.3.0
+ * Version: 1.3.1
  * Author: Cem Firat
  * Requires PHP: 7.4
  * Update URI: https://github.com/cemfirat/ccf-sites-ads-wordpress-connector
@@ -10,7 +10,7 @@
 
 defined('ABSPATH') || exit;
 
-define('CCF_SITES_ADS_PLUGIN_VERSION', '1.3.0');
+define('CCF_SITES_ADS_PLUGIN_VERSION', '1.3.1');
 define('CCF_SITES_ADS_PLUGIN_FILE', __FILE__);
 
 require_once __DIR__ . '/ccf-site-connector-runtime.inc';
@@ -26,6 +26,8 @@ require_once __DIR__ . '/includes/class-ccf-sites-draft-seo.php';
 CCF_Sites_Draft_SEO::init();
 require_once __DIR__ . '/includes/class-ccf-sites-status-overlay.php';
 CCF_Sites_Status_Overlay::init();
+require_once __DIR__ . '/includes/class-ccf-sites-authors.php';
+CCF_Sites_Authors::init();
 
 final class CCF_Sites_Ads_Plugin_Updater {
     private const REPOSITORY = 'cemfirat/ccf-sites-ads-wordpress-connector';
