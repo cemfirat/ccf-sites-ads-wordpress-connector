5534ea76c9f7031c16e6988e832c0ba3e0b613a7
Cem Firat <5923877+cemfirat@users.noreply.github.com>
2026-09-17 13:42:43 +0200

    Bump connector version for scheduling support

--- wordpress/ccf-google-ads-site-connector.php
+++ wordpress/ccf-google-ads-site-connector.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: CCF Sites & Ads Connector
  * Description: Secure WordPress control, content administration and conversion connector for CCF Sites & Ads.
- * Version: 1.2.2
+ * Version: 1.3.0
  * Author: Cem Firat
  * Requires PHP: 7.4
  * Update URI: https://github.com/cemfirat/ccf-sites-ads-wordpress-connector
@@ -10,7 +10,7 @@
 
 defined('ABSPATH') || exit;
 
-define('CCF_SITES_ADS_PLUGIN_VERSION', '1.2.2');
+define('CCF_SITES_ADS_PLUGIN_VERSION', '1.3.0');
 define('CCF_SITES_ADS_PLUGIN_FILE', __FILE__);
 
 require_once __DIR__ . '/ccf-site-connector-runtime.inc';
